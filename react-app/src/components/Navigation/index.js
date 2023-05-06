d3862149afeb02f696d6327eaf1d2b99cc8a0624
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-06 17:18:21 -0400

    styling again from kisha

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -6,37 +6,41 @@ import './Navigation.css';
 import logo from "../../assets/logo-spork.jpeg"
 
 function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+    const sessionUser = useSelector(state => state.session.user);
 
-	return (
+    return (
 
-		<ul className='home-whole-nav-bar'>
-			<div className="left-nav-bar">
-			<NavLink exact to="/"><div className="spork-logo">Spork</div></NavLink>
-			</div>
-			<div className='right-nav-bar'>
-			<li className="home-button-icons">
-				{/* <NavLink exact to="/"><div className="spork-logo"></div></NavLink> */}
-				<NavLink exact to="/"><i className="fa fa-home"></i></NavLink>
-				<NavLink exact to="/"><i className="fa fa-video-camera"></i></NavLink>
-				<NavLink exact to="/"><i className="fa fa-compass"></i></NavLink>
-				<NavLink exact to="/"><i className='fas fa-store-alt'></i></NavLink>
-				<NavLink exact to="/"><i className="fa fa-envelope"></i></NavLink>
-				<NavLink exact to="/"><i className='fas fa-comment-dots'></i></NavLink>
-				<NavLink exact to="/"><i className="fa fa-bolt"></i></NavLink>
-				<NavLink exact to="/"><i className='fas fa-user-alt'></i></NavLink>
-				<NavLink exact to="/"><i className='fas fa-pen-square'></i></NavLink>
+        <ul className='home-whole-nav-bar'>
+            <div className="left-nav-bar">
+            <NavLink exact to="/"><div className="spork-logo">S</div></NavLink>
+            <div className="search-bar">
+                    <input type="text" placeholder= " Search Spork" />
 
-			</li>
-			</div>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
+                </div>
+            </div>
+            <div className='right-nav-bar'>
+            <li className="home-button-icons">
+                {/* <NavLink exact to="/"><div className="spork-logo"></div></NavLink> */}
+                <NavLink exact to="/"><i className="fa fa-home"></i></NavLink>
+                <NavLink exact to="/"><i className="fa fa-video-camera"></i></NavLink>
+                <NavLink exact to="/"><i className="fa fa-compass"></i></NavLink>
+                <NavLink exact to="/"><i className='fas fa-store-alt'></i></NavLink>
+                <NavLink exact to="/"><i className="fa fa-envelope"></i></NavLink>
+                <NavLink exact to="/"><i className='fas fa-comment-dots'></i></NavLink>
+                <NavLink exact to="/"><i className="fa fa-bolt"></i></NavLink>
+                <NavLink exact to="/"><i className='fas fa-user-alt'></i></NavLink>
+                <NavLink exact to="/"><i className='fas fa-pen-square'></i></NavLink>
 
-				</li>
-			)}
-		</ul>
-	);
+            </li>
+            </div>
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={sessionUser} />
+
+                </li>
+            )}
+        </ul>
+    );
 }
 
 export default Navigation;
