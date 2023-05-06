c7119647da67d04fefd05004dd7992f13fde8cde
Brian Washington <bw49078@gmail.com>
2023-05-06 16:51:02 -0500

    tweaked feed

--- react-app/src/components/Posts/Feed/index.js
+++ react-app/src/components/Posts/Feed/index.js
@@ -1,68 +1,34 @@
-import PostItem from "../Post"
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { getAllPosts } from '../../../store/post'
+import PostItem from "../PostItem"
 import "./Feed.css"
 import { NavLink } from "react-router-dom"
 
-const users = [
-    {
-        id: 1,
-        email: "demo@aa.io",
-        avatar: "https://source.unsplash.com/random/50x50"
-    },
-    {
-        id: 2,
-        email: "bruh@gmail.com",
-        avatar: "https://source.unsplash.com/random/50x50"
-    }
-
-]
-
-const posts = [
-    {
-        id: 1,
-        content: "hello",
-        userId: 1,
-        username: "demo",
-        timestamp: "May 4, 2023 8:30 AM"
-    },
-    {
-        id: 2,
-        content: "bye",
-        userId: 2,
-        username: "bruh",
-        timestamp: "May 3, 2023 5:45 PM"
-    }
-]
-
 const Feed = () => {
-    let postArr = Object.values(posts)
-    console.log(postArr)
+    const dispatch = useDispatch()
+    const posts = useSelector(state => state.posts)
+    useEffect(() => {
+        dispatch(getAllPosts())
+    }, [])
+
     return (
         <div className='Feed'>
             <div className="post-option">
-            <NavLink exact to="/"> Aa</NavLink>
-            <NavLink exact to="/"><i class="fa fa-camera"></i></NavLink>
-            <NavLink exact to="/"><i class="fa fa-quote-left"></i></NavLink>
-            <NavLink exact to="/"><i class="fa fa-chain"></i></NavLink>
-            <NavLink exact to="/"><i class='fas fa-comment-dots'></i></NavLink>
-            <NavLink exact to="/"><i class="fa fa-headphones"></i></NavLink>
-            <NavLink exact to="/"><i class="fa fa-video-camera"></i></NavLink>
-            <span></span>
+                <NavLink exact to="/"> Aa</NavLink>
+                <NavLink exact to="/"><i class="fa fa-camera"></i></NavLink>
+                <NavLink exact to="/"><i class="fa fa-quote-left"></i></NavLink>
+                <NavLink exact to="/"><i class="fa fa-chain"></i></NavLink>
+                <NavLink exact to="/"><i class='fas fa-comment-dots'></i></NavLink>
+                <NavLink exact to="/"><i class="fa fa-headphones"></i></NavLink>
+                <NavLink exact to="/"><i class="fa fa-video-camera"></i></NavLink>
+                <span></span>
             </div>
             <ul className='posts'>
-                {postArr.map(post => (
+                {Object.values(posts).map(post => (
                     <li key={post.id} className="post">
-                        <div className="post-header">
-                            <img src={users[post.userId - 1].avatar} alt={users[post.userId - 1].email} />
-                            <span className="username">{post.username}</span>
-                            <span className="timestamp">{post.timestamp}</span>
-                        </div>
-                        <div className="post-content">
-                            {post.content}
-                        </div>
-                        <div className="post-footer">
-                            <button className="like-button">Like</button>
-                            <button className="reblog-button">Reblog</button>
-                        </div>
+                        <PostItem post={post} />
                     </li>
                 ))}
             </ul>
---
+++ react-app/src/components/Posts/PostItem/index.js
@@ -0,0 +1,34 @@
+import "./PostItem.css"
+
+const PostItem = ({ post }) => {
+    return (
+        // <div className="post-header">
+        //     <img src={users[post.userId - 1].avatar} alt={users[post.userId - 1].email} />
+        //     <span className="username">{post.username}</span>
+        //     <span className="timestamp">{post.timestamp}</span>
+        // </div>
+        // <div className="post-content">
+        // {post.content}
+        // </div>
+        // <div className="post-footer">
+        //     <button className="like-button">Like</button>
+        //     <button className="reblog-button">Reblog</button>
+        // </div>
+        <div>
+            <div className="post-header">
+                <img src="https://assets.tumblr.com/images/default_avatar/cone_open_64.png" />
+                <span className="username">{post.owner.username}</span>
+                {/* <span className="timestamp">{post.timestamp}</span> */}
+            </div>
+            <div className="post-content">
+                {post.content}
+            </div>
+            <div className="post-footer">
+                <button className="like-button">Like</button>
+                <button className="reblog-button">Reblog</button>
+            </div>
+        </div>
+    )
+}
+
+export default PostItem
