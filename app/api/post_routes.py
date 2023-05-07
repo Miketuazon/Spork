1df7c1f030e982a2125d09fcd892e7b71989751e
Brian Washington <bw49078@gmail.com>
2023-05-07 13:05:40 -0500

    create route in post backend fixed

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -28,6 +28,7 @@ def current_user_posts():
     """
     Query for all user's posts and returns them in a list of post dictionaries
     """
+
     current_user_dict = current_user.to_dict()
     posts = Post.query.filter(Post.userId == current_user_dict['id'])
     return_list = []
@@ -43,15 +44,18 @@ def current_user_posts():
 @login_required
 def create_a_post():
     form = PostForm()
+    current_user_dict = current_user.to_dict()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         new_post = Post(
             content=form.data['content'],
-            userId=form.data('userId')
+            userId=current_user_dict['id']
         )
         db.session.add(new_post)
         db.session.commit()
+        return {"Successfully Created Post": new_post.to_dict()}
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 
 @post_routes.route('/delete/<id>', methods=['DELETE'])
