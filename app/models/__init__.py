e053b29e2e761401d9d81cc34998dc11bc620df3
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-08 14:06:36 -0400

    added last commit from brian

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,5 @@ from .user import User
 from .post import Post
 from .db import environment, SCHEMA
 from .likes import Like
-<<<<<<< HEAD
 from .follows import Follow
-=======
 from .comment import Comment
->>>>>>> dev
