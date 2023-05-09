aa1cb56e1fc8ac97b97f750353111fb25d3f043d
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-09 12:53:13 -0400

    env changes

--- app/models/follows.py
+++ app/models/follows.py
@@ -3,8 +3,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 follows = db.Table(
     'follows',
     db.Model.metadata,
-    db.Column('follower_id', db.Integer, db.ForeignKey("users.id"), nullable=False),
-    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'), nullable=False)
+    db.Column('follower_id', db.Integer, db.ForeignKey("users.id"), nullable=True),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'), nullable=True)
 )
 
 if environment == "production":
