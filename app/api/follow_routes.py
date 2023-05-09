2b88f0a95677562068089e1741e2b9293add493b
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-09 19:11:56 -0400

    added 1 migration

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, session, request
+from flask import Blueprint, jsonify, session, request, redirect
 from app.models import User, db, Post, Like, Comment, follows
 from .auth_routes import validation_errors_to_error_messages
 from app.forms import PostForm, CommentForm
@@ -7,20 +7,12 @@ from datetime import date
 
 follow_routes = Blueprint('follow', __name__)
 
-@follow_routes.route('/followers')
-@login_required
-def get_followers():
-    """
-    Query for getting a user's followers
-    """
-    current_user_dict = current_user.todict()
-    pass
-
-@follow_routes.route('/follow', methods=['POST'])
+@follow_routes.route('/', methods=['GET'])
 @login_required
 def follow_a_user():
     """
     Query for following a user
     """
     current_user_dict = current_user.todict()
-    followed_user = User.query.filter_by(id=followed_user_id)
+    print(current_user_dict)
+    return redirect('/')
--- app/models/follows.py
+++ app/models/follows.py
@@ -2,6 +2,6 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 follows = db.Table(
     "follows",
-    db.Column("follower_id", db.Integer, db.ForeignKey("users.id")),
-    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"))
+    db.Column("follower_id", db.Integer, db.ForeignKey("users.id"), nullable=False),
+    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"), nullable=False)
 )
