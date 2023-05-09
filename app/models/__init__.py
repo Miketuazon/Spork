0f8da9763c282a9dfa919cd2223bf42fa2585e04
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-09 12:36:45 -0400

    another main fix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,5 @@ from .user import User
 from .post import Post
 from .db import environment, SCHEMA
 from .likes import Like
-from .follows import follows
+# from .follows import follows
 from .comment import Comment
