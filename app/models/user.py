96c7bf6d971c1c42217ae7967a9057d9940b2647
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-09 13:05:44 -0400

    nuked follows

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from .follows import follows
-#
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -18,15 +17,6 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', back_populates='owner')
     likes = db.relationship('Like', back_populates='user')
     comments = db.relationship('Comment', back_populates='comment_owner')
-
-    following = db.relationship(
-        'User',
-        secondary=follows,
-        primaryjoin=(follows.c.follower_id == id),
-        secondaryjoin=(follows.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'),
-    )
-
 
     @property
     def password(self):
@@ -44,6 +34,4 @@ class User(db.Model, UserMixin):
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'following': [user.id for user in self.following],
-            'followers': [user.id for user in self.followers]
         }
