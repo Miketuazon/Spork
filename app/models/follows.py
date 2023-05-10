731f2934a5e38d15d881bc85e30f156732503e4f
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-10 13:50:02 -0400

    added prefix_for_prod on followres and likes tables

--- app/models/follows.py
+++ app/models/follows.py
@@ -2,8 +2,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 follows = db.Table(
     "follows",
-    db.Column("follower_id", db.Integer, db.ForeignKey("users.id"), nullable=False),
-    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"), nullable=False)
+    db.Column("follower_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False),
+    db.Column("followed_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
 )
 
 if environment == "production":
--- app/models/likes.py
+++ app/models/likes.py
@@ -5,13 +5,13 @@ likes = db.Table(
     db.Column(
         "userId",
         db.Integer,
-        db.ForeignKey("users.id"),
+        db.ForeignKey(add_prefix_for_prod("users.id")),
         primary_key=True
     ),
     db.Column(
         "postId",
         db.Integer,
-        db.ForeignKey("posts.id"),
+        db.ForeignKey(add_prefix_for_prod("posts.id")),
         primary_key=True
     )
 )
