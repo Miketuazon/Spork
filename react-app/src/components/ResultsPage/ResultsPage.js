7de67f6aeadcf310589207b26e4a0571905a4e08
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-11 18:51:17 -0400

    added background color in public index.html and content?.includes query in resultspage.js

--- react-app/src/components/ResultsPage/ResultsPage.js
+++ react-app/src/components/ResultsPage/ResultsPage.js
@@ -25,7 +25,7 @@ function ResultsPage() {
             <ul className='posts'>
                 {
                     Object?.values(posts)?.map(post => (
-                        post.content.includes(query)
+                        post.content?.includes(query) || post.title?.includes(query)
                             ?
                             <li key={post?.id} className="post">
                                 <PostItem post={post} />
