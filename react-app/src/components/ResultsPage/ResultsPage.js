2afe34fa88e19adb61886b463f29a9043d58903d
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-12 17:47:27 -0400

    made more seed data and fixed search bar

--- react-app/src/components/ResultsPage/ResultsPage.js
+++ react-app/src/components/ResultsPage/ResultsPage.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { getAllPosts } from '../../store/post'
 import PostItem from "../Posts/PostItem"
-// import "./Feed.css"
 import { NavLink, useLocation } from "react-router-dom"
 
 function ResultsPage() {
@@ -16,15 +15,40 @@ function ResultsPage() {
     const posts = useSelector(state => state?.posts)
     const postsVal = Object?.values(posts)
     console.log('Posts', posts)
+
+    // Creating state and function for sorting posts
+    const [sortOrder, setSortOrder] = useState('desc');
+    const sortedPosts = Object.values(posts).sort((a, b) => {
+        if (sortOrder === 'asc') {
+            return a.createdAt.localeCompare(b.createdAt);
+        } else {
+            return b.createdAt.localeCompare(a.createdAt);
+        }
+    });
+
+    function handleSortClick() {
+        if (sortOrder === 'asc') {
+            setSortOrder('desc');
+        } else {
+            setSortOrder('asc');
+        }
+    }
+
     useEffect(() => {
         dispatch(getAllPosts())
 
     }, [dispatch, JSON.stringify(postsVal)])
     return (
         <div className='results-of-search'>
+            <div className='sort-container'>
+                <h2>Sort by:</h2>
+                <button onClick={handleSortClick}>
+                    {sortOrder === 'asc' ? 'Older' : 'Most Recent Posts'}
+                </button>
+            </div>
             <ul className='posts'>
                 {
-                    Object?.values(posts)?.map(post => (
+                    sortedPosts?.map(post => (
                         (post?.content?.toLowerCase())?.includes(query) || (post?.title?.toLowerCase())?.includes(query)
                             ?
                             <li key={post?.id} className="post">
