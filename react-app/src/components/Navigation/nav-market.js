37d2b478d05577205c6958b610efff56bf31bab2
Derrick-Truong <dtruong169@gmail.com>
2023-05-13 17:44:45 -0700

    fixed login and button notifcations

--- react-app/src/components/Navigation/nav-market.js
+++ react-app/src/components/Navigation/nav-market.js
@@ -6,17 +6,18 @@ const NavMarket = () => {
     <div className="tumblr-mart-nav">
       <div className="categories-nav-market">
         <button className="category-nav active-nav" data-key="all">All</button>
-        <button className="category-nav" data-key="merch">Merch</button>
-        <button className="category-nav" data-key="gift">Gifts</button>
+        <button onClick={() => alert('Coming soon!')} className="category-nav" data-key="merch">Merch</button>
+        <button onClick={() => alert('Coming soon!')} className="category-nav" data-key="gift">Gifts</button>
       </div>
       <div className="products-nav-market">
         <div className="product-nav-market">
           <div className="banner-nav popular-nav">POPULAR!</div>
           <div className="image-nav-market"><img src="https://assets.tumblr.com/images/tumblrmart/rainbow-checkmark/card_web.gif" alt="Spork product card banner" /></div>
           <div className="details-nav-market">
-            <h2 className="title-nav-market">Important Rainbow Internet Checkmarks</h2>
+            <h3 className="title-nav-market">Important Checkmarks</h3>
             <p className="description-nav-market">Important just got colorful!</p>
-            <button className="shop-nav-market">Shop</button>
+<br></br>
+            <button onClick={() => alert('Coming soon!')} className="shop-nav-market">Shop</button>
 
           </div>
         </div>
@@ -24,9 +25,11 @@ const NavMarket = () => {
           <div className="banner-nav new-nav-market">NEW!</div>
           <div className="image-nav-market"><img src="https://i0.wp.com/shop.tumblr.com/wp-content/uploads/2023/04/TumblrUniversity.jpg?resize=150%2C150&amp;ssl=1" alt="TumblrMart product card banner" /></div>
           <div className="details-nav-market">
-            <h2 className="title-nav-market">Tumblr University T-Shirt</h2>
+            <h2 className="title-nav-market">Spork Uni T-Shirt</h2>
+            <br></br>
             <p className="description-nav-market">Celebrate your love for Tumblr with this comfortable t-shirt!</p>
-            <button className="shop-nav-market">Shop</button>
+            <br></br>
+            <button onClick={() => alert('Coming soon!')} className="shop-nav-market">Shop</button>
 
           </div>
         </div>
--- react-app/src/components/Posts/PostItem/index.js
+++ react-app/src/components/Posts/PostItem/index.js
@@ -21,12 +21,12 @@ const PostItem = ({ post }) => {
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state?.session?.user)
     const currentFollowing = currentUser?.following
-    const postFollowers = Object?.values(post?.owner?.followers)
+    const postFollowers = post?.owner?.followers
     const postsVal = Object.values(post)
     const comments = useSelector(state => state?.comments)
     const notes = Number(post?.comments?.length + post?.likes?.length)
-    const postComments = Object.values(post?.comments)
-    const postLikes = Object?.values(post?.likes)
+    const postComments = post?.comments
+    const postLikes = post?.likes
 
     const follower = post?.owner?.followers?.find(id => id === currentUser?.id)
     console.log('follower', follower)
@@ -109,7 +109,7 @@ const PostItem = ({ post }) => {
             <div className="post-footer">
                 <button onClick={openMenu} className="like-button">{notes === 1 ? <div><span>{notes} </span><span>note</span></div> : <div><span>{notes} </span><span>notes</span></div>}</button>
                 {currentUser && !liked && (currentUser?.id !== post?.userId) ? <button className="like-button" onClick={onSubmitLike}><i className="far fa-heart"></i></button> : currentUser && liked && (currentUser?.id !== post?.userId) ? <button className="unlike-button" onClick={onSubmitLike}><i className="fas fa-heart" ></i></button> : <></>}
-                <button className="reblog-button"><i class="fa fa-retweet"></i></button>
+                {currentUser ? <button onClick={() => alert('Coming soon!')} className="reblog-button"><i class="fa fa-retweet"></i></button>:<></>}
                 {/* {currentUser?.id === post?.userId ? (<><OpenModalButton
                     buttonText={<><i className="fa fa-pen-square"></i></>}
                     modalComponent={<EditPost postId={post?.id} post={post} />}
@@ -123,7 +123,7 @@ const PostItem = ({ post }) => {
                         />
 
                         <OpenModalButton
-                            buttonText={<><i className="fa fa-pen-square"></i></>}
+                            buttonText={<><i className="fa fa-pencil"></i></>}
                             modalComponent={<EditPost postId={post?.id} post={post} />}
                         />
                     </div>
