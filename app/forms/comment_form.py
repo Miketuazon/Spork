85ccaa90fc2fe8b0adfd452b61f35a4b62b1bdf9
Brian <bw49078@gmail.com>
2024-05-05 16:18:11 -0500

    fixed comment error handler

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -5,6 +5,6 @@ from app.models import User, Post, Comment
 
 
 class CommentForm(FlaskForm):
-    content = StringField('content', validators=[DataRequired(), Length(min=3,max=256, message='Content must be between 3 and 255 characters')] )
+    content = StringField('content', validators=[DataRequired(message="Comment must be between 3 and 255 characters!"), Length(min=3,max=256, message='Comment must be between 3 and 255 characters!')] )
     userId = IntegerField('owner')
     postId = IntegerField('post')
--- react-app/src/components/comments/CreateComment/index.js
+++ react-app/src/components/comments/CreateComment/index.js
@@ -1,28 +1,24 @@
-import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { thunkCreateComment } from "../../../store/post";
 import { useState } from "react";
-import { useSelector } from "react-redux";
 import { useModal } from "../../../context/Modal";
 import "./CreateComment.css"
 
 export default function CreateComment({ postId }) {
-    const history = useHistory();
     const dispatch = useDispatch();
     const { closeModal } = useModal();
-    const currentUser = useSelector(state => state?.session?.user);
     const [content, setContent] = useState('');
     const [errors, setErrors] = useState([]);
 
     const onSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        setErrors([]);
         const newComment = {
             content: content
         }
         const successComment = await dispatch(thunkCreateComment(newComment, postId))
         if (successComment.errors) {
-            setErrors(successComment)
+            setErrors(successComment.errors)
         } else {
             setContent('')
             closeModal()
@@ -39,8 +35,6 @@ export default function CreateComment({ postId }) {
             <div className="user-picture-comments">
                 <img className="user-cone-comments" src="https://assets.tumblr.com/images/default_avatar/cone_open_64.png" alt="default_image.png" />
                 <form className="create-post-area-comments-form" onSubmit={onSubmit}>
-
-
                     <textarea className="create-post-textarea-comments"
                         rows="1"
                         cols="10"
