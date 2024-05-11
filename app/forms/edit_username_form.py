efdc5ac0f27c16389abc06faa6a4abd47c3448ad
Brian <bw49078@gmail.com>
2024-05-11 05:18:59 -0500

    added edit username form

---
+++ app/forms/edit_username_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError, Length
+from app.models import User
+
+def username_exists(form, field):
+    # Checking if username is already in use
+    username = field.data
+    user = User.query.filter(User.username == username).first()
+    if user:
+        raise ValidationError('Username is already in use.')
+    
+
+class UserNameForm(FlaskForm):
+    username = StringField(
+        'username', validators=[DataRequired(), username_exists, Length(min=3, max=26, message='Username must be between 3 and 25 characters') ])
