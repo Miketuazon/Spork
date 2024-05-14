adffdb4bfe0b2a956276cb02752eec192a0b0f2e
Miketuazon <michael.d.tuazon@gmail.com>
2024-05-14 12:15:11 -0400

    added dropdown. need to still add functionality

--- react-app/src/components/SideMenu/index.js
+++ react-app/src/components/SideMenu/index.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./SideMenu.css"
 
 const SideMenu = () => {
+
+    const dispatch = useDispatch();
+    const [showAccount, setShowAccount] = useState(false)
+    const [showAccountValue, setShowValue] = useState("Show Account")
+
+    // code for arrow up/down depending on click
+    const handleAccountClick = () => {
+        if (showAccountValue === "Show Account") {
+            setShowValue("Hide Account")
+            setShowAccount(true)
+        }
+        else {
+            setShowValue("Show Account")
+            setShowAccount(false)
+        }
+    }
+
+    // Code to hide and show account dropdown
+    const showAccountDiv = "account-container" + (showAccount ? "" : " hidden")
 
     return (
         <>
@@ -20,11 +41,16 @@ const SideMenu = () => {
                         <div className="sideIcon">
                             <i class="fa fa-user" aria-hidden="true"></i>
                         </div>
-                        <div className="account">
+                        <div className="account" onClick={handleAccountClick}>
                             Account
                             <div><i class="fa fa-caret-down" aria-hidden="true"></i></div>
                         </div>
                     </li>
+                    <div className={showAccountDiv}>
+                        <div className="likes-account">Likes</div>
+                        <div className="following-account">Following</div>
+                        <div className="logOutButton-account">Log Out</div>
+                    </div>
                 </ul>
                 <div className="createPostButtonContainer">
                     <button className="createPostButton">
