1ee90d8cb01cc84999420c0a66214079a2f25236
Miketuazon <michael.d.tuazon@gmail.com>
2024-05-14 11:12:41 -0400

    created home and account side components. need to add functionality to account dropdown and create button

--- react-app/src/components/SideMenu/index.js
+++ react-app/src/components/SideMenu/index.js
@@ -7,12 +7,23 @@ const SideMenu = () => {
             <div className="sideMenu">
                 <ul className="categoriesSideMenu">
                     <div className="sporkLogoSide"></div>
-                    <li className="HomeButton">
+                    <div className="HomeButton">
                         <h1 className="sporkLogoSideMenu">Spork</h1>
+                    </div>
+                    <li className="homeDropdown">
+                        <div className="sideIcon">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </div>
+                        <div className="sideTitle">Home</div>
                     </li>
                     <li className="accountDropdown">
-                        <div>Account</div>
-                        <div><i class="fa fa-caret-down" aria-hidden="true"></i></div>
+                        <div className="sideIcon">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </div>
+                        <div className="account">
+                            Account
+                            <div><i class="fa fa-caret-down" aria-hidden="true"></i></div>
+                        </div>
                     </li>
                 </ul>
                 <div className="createPostButtonContainer">
