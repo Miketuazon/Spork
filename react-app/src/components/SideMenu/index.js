9cc9287ce40858faf2104fec39ac41e9421f74ca
Miketuazon <michael.d.tuazon@gmail.com>
2024-05-14 12:58:30 -0400

    added showing login button if user not signed in.

--- react-app/src/components/SideMenu/index.js
+++ react-app/src/components/SideMenu/index.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./SideMenu.css"
 import { logout } from "../../store/session";
 import { useHistory } from "react-router-dom";
 import OpenModalButton from "../OpenModalButton";
 import CreatePost from "../Posts/CreatePost";
+import LoginButton from "../Navigation/LoginButton";
+import LoginFormModal from "../LoginFormModal";
 
 const SideMenu = () => {
+    const sessionUser = useSelector(state => state?.session?.user);
     const history = useHistory();
     const dispatch = useDispatch();
     const [showAccount, setShowAccount] = useState(false)
@@ -34,43 +37,66 @@ const SideMenu = () => {
     // Code to hide and show account dropdown
     const showAccountDiv = "account-container" + (showAccount ? "" : " hidden")
 
+    useEffect(() => {
+
+    }, [sessionUser])
+
     return (
         <>
-            <div className="sideMenu">
-                <ul className="categoriesSideMenu">
-                    <div className="sporkLogoSide"></div>
-                    <div className="HomeButton">
-                        <h1 className="sporkLogoSideMenu">Spork</h1>
+            {sessionUser ? (
+                <div className="sideMenu">
+                    <ul className="categoriesSideMenu">
+                        <div className="sporkLogoSide"></div>
+                        <div className="HomeButton">
+                            <h1 className="sporkLogoSideMenu">Spork</h1>
+                        </div>
+                        <li className="homeDropdown" onClick={() => window.location.href = '/'}>
+                            <div className="sideIcon">
+                                <i class="fa fa-home" aria-hidden="true"></i>
+                            </div>
+                            <div className="sideTitle">Home</div>
+                        </li>
+                        <li className="accountDropdown">
+                            <div className="sideIcon">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </div>
+                            <div className="account" onClick={handleAccountClick}>
+                                Account
+                                <div><i class="fa fa-caret-down" aria-hidden="true"></i></div>
+                            </div>
+                        </li>
+                        <div className={showAccountDiv}>
+                            <div className="likes-account" onClick={() => window.location.href = '/Likes'}>Likes</div>
+                            <div className="following-account" onClick={() => window.location.href = "/following"}>Following</div>
+                            <div className="logOutButton-account" onClick={handleLogout}>Log Out</div>
+                        </div>
+                    </ul>
+                    <div className="createPostButtonContainer">
+                        <OpenModalButton className="createPostButton" buttonText={<button className="createPostButton">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                            Create
+                        </button>} modalComponent={<CreatePost />}>
+                        </OpenModalButton>
                     </div>
-                    <li className="homeDropdown" onClick={() => window.location.href = '/'}>
-                        <div className="sideIcon">
-                            <i class="fa fa-home" aria-hidden="true"></i>
-                        </div>
-                        <div className="sideTitle">Home</div>
-                    </li>
-                    <li className="accountDropdown">
-                        <div className="sideIcon">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </div>
-                        <div className="account" onClick={handleAccountClick}>
-                            Account
-                            <div><i class="fa fa-caret-down" aria-hidden="true"></i></div>
-                        </div>
-                    </li>
-                    <div className={showAccountDiv}>
-                        <div className="likes-account" onClick={() => window.location.href = '/Likes'}>Likes</div>
-                        <div className="following-account" onClick={() => window.location.href = "/following"}>Following</div>
-                        <div className="logOutButton-account" onClick={handleLogout}>Log Out</div>
-                    </div>
-                </ul>
-                <div className="createPostButtonContainer">
-                    <OpenModalButton className="createPostButton" buttonText={<button className="createPostButton">
-                        <i class="fa fa-pencil" aria-hidden="true"></i>
-                        Create
-                    </button>} modalComponent={<CreatePost />}>
-                    </OpenModalButton>
                 </div>
-            </div>
+
+            ) : (
+                <div className="sideMenu">
+                    <ul className="categoriesSideMenu">
+                        <div className="sporkLogoSide"></div>
+                        <div className="HomeButton">
+                            <h1 className="sporkLogoSideMenu">Spork</h1>
+                        </div>
+                        <div className="login-sidemenu">
+                            <LoginButton
+                                clasName="button-green-login"
+                                buttonText="Log In"
+                                modalComponent={<LoginFormModal />}
+                            />
+                        </div>
+                    </ul>
+                </div>
+            )}
         </>
     )
 }
