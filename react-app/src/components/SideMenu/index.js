1f853878bd1d0d2922d9a2b3456edf0e1a5d9b1d
Brian <bw49078@gmail.com>
2024-05-16 11:17:41 -0500

    added posts and follower buttons to side menu

--- react-app/src/components/SideMenu/index.js
+++ react-app/src/components/SideMenu/index.js
@@ -67,8 +67,9 @@ const SideMenu = () => {
                             </div>
                         </li>
                         <div className={showAccountDiv}>
-                            <div className="likes-account" onClick={() => window.location.href = '/Likes'}>Likes</div>
+                            <div  className="following-account" onClick={() => window.location.href = "/posts"}>Posts</div>
                             <div className="following-account" onClick={() => window.location.href = "/following"}>Following</div>
+                            <div className="following-account" onClick={() => window.location.href = "/followers"}>Followers</div>
                             <div className="logOutButton-account" onClick={handleLogout}>Log Out</div>
                         </div>
                     </ul>
