d5c1f4ad937a1dedd7635ef26cb0546b9c8b9f9f
Brian <bw49078@gmail.com>
2024-05-16 04:56:58 -0500

    if user is not logged in they cannot go to likes page

--- react-app/src/components/Likes/AllLikes.js
+++ react-app/src/components/Likes/AllLikes.js
@@ -1,22 +1,44 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { thunkGetAllPosts } from "../../store/post";
+import { useHistory } from "react-router-dom";
 import PostItem from "../Posts/PostItem";
+import LoadingScreen from "../LoadingScreen";
 import "./AllLikes.css"
+import "../Posts/Feed/Feed.css"
 
 export default function AllLikes() {
-    const sessionUser = useSelector(state => state?.session.user)
     const dispatch = useDispatch()
-
+    const history = useHistory()
+    const sessionUser = useSelector(state => state?.session.user)
+    const posts = useSelector(state => state.posts.allPosts);
 
     useEffect(() => {
-
+        dispatch(thunkGetAllPosts());
     }, [dispatch])
+
+    if (!sessionUser) history.push('/')
 
     return (
         <>
-            <div>
-                <h1 className="all-likes-header">All Likes</h1>
+        <div className="Feed">
+          {posts ? (
+              <ul className="posts">
+                {Object.values(posts).sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))?.map(post =>
+                  (
+                    <li key={post.id} className="post">
+                      <PostItem post={post} />
+                    </li>
+                  ))}
+              </ul>
+          ) : (
+          <>
+            <div className="loading">
+              <LoadingScreen />
             </div>
+          </>
+          )}
+        </div>
         </>
-    )
+      )
 }
