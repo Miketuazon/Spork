7839edbab5ebdbdf8ae1527f29dea14d203e8b2b
Brian <bw49078@gmail.com>
2024-05-26 12:54:16 -0500

    added team members names to bottom of search bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -27,46 +27,6 @@ function Navigation({ isLoaded }) {
 				<div className="search-bar">
 					< SearchBar />
 				</div>
-				<div className='footer'>
-					<ul className='group-link'>
-						<div className='brian-link'>
-						<li className='name'>
-							Brian Washington
-						</li>
-						<li className='link'>
-							<a target='_blank' href='https://www.linkedin.com/in/brian-washington-668129244/'><i class="fa fa-linkedin-square"></i></a>
-							<a target='_blank' href='https://github.com/zipzopboppitybop' className='end'><i class="fa fa-github"></i></a>
-						</li>
-						</div>
-						<div className='derrick-link'>
-						<li className='name'>
-							Derrick Truong
-						</li>
-						<li className='link'>
-							<a target='_blank' href='https://www.linkedin.com/in/derrick-truong-1a092121a/'><i class="fa fa-linkedin-square"></i></a>
-							<a target='_blank' href='https://github.com/Derrick-Truong' className='end'><i class="fa fa-github"></i></a>
-						</li>
-						</div>
-						<div className='kisha-link'>
-						<li className='name'>
-							Kisha Onia
-						</li>
-						<li className='link'>
-							<a target='_blank' href='https://www.linkedin.com/in/kisha-rose-onia-63bb35182/'><i class="fa fa-linkedin-square"></i></a>
-							<a target='_blank' href='https://github.com/kishaonia' className='end'><i class="fa fa-github"></i></a>
-						</li>
-						</div>
-						<div className='michael-link'>
-						<li className='name'>
-							Michael Tuazon
-						</li>
-						<li className='link'>
-							<a target='_blank' href='https://www.linkedin.com/in/miketuazon/'><i class="fa fa-linkedin-square"></i></a>
-							<a target='_blank' href='https://github.com/Miketuazon' className='end'><i class="fa fa-github"></i></a>
-						</li>
-						</div>
-					</ul>
-				</div>
 			</div>
 			{sessionUser && isLoaded ? (
 				<div className='right-nav-bar'>
--- react-app/src/components/ResultsPage/Searchbar.js
+++ react-app/src/components/ResultsPage/Searchbar.js
@@ -8,22 +8,64 @@ function SearchBar() {
     };
 
     return (
-        <div className='search-bar'>
-            <input
-                className="spork-search-bar"
-                type="text"
-                value={query}
-                placeholder=" Search Spork"
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyPress={(e) => {
-                    if (e.key === 'Enter') {
-                        handleSearch();
-                    }
-                }}
-            />
-            <button className="spork-search-button" onClick={handleSearch}><i className="fa">&#xf002;</i></button>
-
-        </div>
+        <>
+            <div className='search-bar'>
+                <input
+                    className="spork-search-bar"
+                    type="text"
+                    value={query}
+                    placeholder=" Search Spork"
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
+                />
+                <button className="spork-search-button" onClick={handleSearch}><i className="fa">&#xf002;</i></button>
+            </div>
+            <div className='footer'>
+                    <h1 className='footer-title'>Meet the Team!</h1>
+					<ul className='group-link'>
+						<div className='link'>
+						<li className='name'>
+							Brian Washington
+                            <div>
+                                <a target='_blank' href='https://www.linkedin.com/in/brian-washington-668129244/'><i className="fa fa-linkedin-square user-icon"></i></a>
+							    <a target='_blank' href='https://github.com/zipzopboppitybop' ><i className="fa fa-github user-icon"></i></a>
+                            </div>
+						</li>
+						</div>
+						<div className='link'>
+						<li className='name'>
+							Derrick Truong
+                            <div>
+                                <a target='_blank' href='https://www.linkedin.com/in/derrick-truong-1a092121a/'><i className="fa fa-linkedin-square user-icon"></i></a>
+							    <a target='_blank' href='https://github.com/Derrick-Truong'><i className="fa fa-github user-icon"></i></a>
+                            </div>
+						</li>
+						</div>
+						<div className='link'>
+						<li className='name'>
+							Kisha Onia
+                            <div>
+                                <a target='_blank' href='https://www.linkedin.com/in/kisha-rose-onia-63bb35182/'><i className="fa fa-linkedin-square user-icon"></i></a>
+							    <a target='_blank' href='https://github.com/kishaonia' ><i className="fa fa-github user-icon"></i></a>
+                            </div>
+						</li>
+						</div>
+						<div className='link'>
+						<li className='name'>
+							Michael Tuazon
+                            <div>
+                                <a target='_blank' href='https://www.linkedin.com/in/miketuazon/'><i className="fa fa-linkedin-square user-icon"></i></a>
+							    <a target='_blank' href='https://github.com/Miketuazon' ><i className="fa fa-github user-icon"></i></a>
+                            </div>
+						</li>
+						</div>
+					</ul>
+				</div>
+        </>
     );
 }
 
