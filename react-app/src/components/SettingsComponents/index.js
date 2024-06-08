c8e3f0ee63d94c257ee6e4e329858de7c90c3c63
Brian <bw49078@gmail.com>
2024-06-08 12:28:48 -0500

    settings page component added to app.js

---
+++ react-app/src/components/SettingsComponents/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import "./SettingsPage.css";
+
+const SettingsPage = () => {
+
+  return (
+    <div id='settings-page'>
+        <h1>Settings Page</h1>
+    </div>
+  )
+}
+
+export default SettingsPage
